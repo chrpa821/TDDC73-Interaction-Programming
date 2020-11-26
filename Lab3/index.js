2e3890956562535a97e88f5fb51366995682d4b3
chrpa821 <chrispaulusson@gmail.com>
2020-11-26 16:34:22 +0100

    add exchangeRates component

--- Lab3/index.js
+++ Lab3/index.js
@@ -1,9 +1,18 @@
-/**
- * @format
- */
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import MyApp from './App'
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
 
-AppRegistry.registerComponent(appName, () => App);
+const App = () => (
+  <ApolloProvider client={client}>
+    <MyApp/>
+  </ApolloProvider>
+);
+
+AppRegistry.registerComponent('Lab3', () => App);
