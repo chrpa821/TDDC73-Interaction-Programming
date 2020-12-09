6a68ccd45cb1cf6c078a91ee07295c1a06da9cbe
chrpa821 <chrispaulusson@gmail.com>
2020-12-09 10:18:03 +0100

    add HomeScreen component

--- Lab3/App.js
+++ Lab3/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useQuery, gql } from '@apollo/client';
 
 import HomeScreen from './src/HomeScreen'
 
@@ -10,53 +9,14 @@ import {
   Text,
 } from 'react-native';
 
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
-
-const REPO_QUERY = gql`
-  query{
-    search(type: REPOSITORY, first: 10, query: "language: any") {
-      nodes {
-        ... on Repository {
-          name
-          stargazerCount
-          id
-        }
-      }
-    }
-  }
-`;
-
-function Repos() {
-const { loading, error, data } = useQuery(REPO_QUERY);
-
-if (loading) return <Text>Loading...</Text>
-if (error) return <Text>Error :(</Text>
-
-return data.search.nodes.map(({ name, stargazerCount }) => (
-  <View key={name}>
-    <Text>
-      {name}: {stargazerCount}
-    </Text>
-  </View>
-));
-}
-
 const App = () => {
 
   return (
     <SafeAreaView>
-      {Repos()}
+      <HomeScreen/>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-});
 
 export default App;
 
--- Lab3/src/HomeScreen.js
+++ Lab3/src/HomeScreen.js
@@ -2,8 +2,26 @@ import React from 'react'
 import { Text, FlatList, Pressable } from 'react-native'
 import { gql, useQuery } from '@apollo/client'
 
-import styles from './styles'
+//import styles from './styles'
 
+import {
+    StyleSheet,
+    View,
+  } from 'react-native';
+
+const REPO_QUERY = gql`
+  query{
+    search(type: REPOSITORY, first: 10, query: "language: any") {
+      nodes {
+        ... on Repository {
+          name
+          stargazerCount
+          id
+        }
+      }
+    }
+  }
+`;
 
 function Repos() {
     const { loading, error, data } = useQuery(REPO_QUERY);
@@ -11,7 +29,7 @@ function Repos() {
     if (loading) return <Text>Loading...</Text>
     if (error) return <Text>Error :(</Text>
     
-    return data.search.nodes.map(({ name, stargazerCount }) => (
+    return data.search.nodes.map(({ name, stargazerCount, id }) => (
       <View key={name}>
         <Text>
           {name}: {stargazerCount}
@@ -23,8 +41,6 @@ function Repos() {
 export default () => {
 
     return (
-        <SafeAreaView>
-          {Repos()}
-        </SafeAreaView>
+        Repos()
       );
   }
